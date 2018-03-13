d9b5909c90a0235e1c4cdb07008cf0bdd3b5af64
jompons <snookerace6211@gmail.com>
2018-03-13 17:26:36 +0700

    Add view

--- app/src/main/java/com/jompon/environment/sample/MainActivity.java
+++ app/src/main/java/com/jompon/environment/sample/MainActivity.java
@@ -19,12 +19,18 @@ package com.jompon.environment.sample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtEnvironment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtEnvironment = (TextView) findViewById(R.id.txt_environment);
+        txtEnvironment.setText(R.string.app_name);
     }
 }
